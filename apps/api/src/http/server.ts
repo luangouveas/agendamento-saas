1bf8d55026243a22100414e75d119d6053006af6
Luan Gouveas <lgouveas@rhmed.com.br>
2025-02-24 14:21:40 -0300

    Cria servidor http da api

---
+++ apps/api/src/http/server.ts
@@ -0,0 +1,54 @@
+import { env } from '@agendamento-saas/env'
+import fastifyCors from '@fastify/cors'
+import { fastifyJwt } from '@fastify/jwt'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import fastify from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import { errorHandler } from './error-handler'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+app.setErrorHandler(errorHandler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js Agendador SaaS',
+      description: 'Agendador Full-stack SaaS app with multi-tenant & RABC.',
+      version: '1.0.0',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyCors)
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
+
+app.listen({ port: env.SERVER_PORT }).then(() => {
+  console.log(`HTTP Server running on http://localhost:${env.SERVER_PORT}/docs`)
+})
