0986dd177a39861cce7d7d87ca1362972504ebbc
Luan Gouveas <lgouveas@rhmed.com.br>
2025-02-26 17:44:35 -0300

    Inclui subject para usuario

--- packages/auth/src/subjects/usuario.ts
+++ packages/auth/src/subjects/usuario.ts
@@ -1,7 +1,12 @@
 import { z } from 'zod'
 
 export const usuarioSubject = z.tuple([
-  z.union([z.literal('manage'), z.literal('get'), z.literal('update')]),
+  z.union([
+    z.literal('manage'),
+    z.literal('get'),
+    z.literal('create'),
+    z.literal('update'),
+  ]),
   z.literal('Usuario'),
 ])
 
