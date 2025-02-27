444cbfd8d8e67b1c45149c2c60d3c8b82fe650d0
Luan Gouveas <lgouveas@rhmed.com.br>
2025-02-27 00:28:09 -0300

    Remove registro de middleware não utilizado

--- apps/api/src/http/routes/auth/autenticar-com-otp.ts
+++ apps/api/src/http/routes/auth/autenticar-com-otp.ts
@@ -1,104 +1,100 @@
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
-import z from 'zod'
+import { z } from 'zod'
 
-import { auth } from '@/http/middlewares/auth'
 import { prisma } from '@/lib/prisma'
 
 import { BadRequestError } from '../_errors/bad-request-error'
 import { UnauthorizedError } from '../_errors/unauthorized-error'
 
 export function AutenticarComOtp(app: FastifyInstance) {
-  app
-    .withTypeProvider<ZodTypeProvider>()
-    .register(auth)
-    .post(
-      '/auth/otp',
-      {
-        schema: {
-          tags: ['Auth'],
-          summary: 'Realiza autenticação via OTP',
-          body: z.object({
-            telefone: z.number(),
-            tokenOtp: z.number(),
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/auth/otp',
+    {
+      schema: {
+        tags: ['Auth'],
+        summary: 'Realiza autenticação via OTP',
+        body: z.object({
+          telefone: z.number(),
+          tokenOtp: z.number(),
+        }),
+        params: z.object({
+          slug: z.string(),
+        }),
+        response: {
+          201: z.object({
+            token: z.string(),
           }),
-          params: z.object({
-            slug: z.string(),
-          }),
-          response: {
-            201: z.object({
-              token: z.string(),
-            }),
-          },
         },
       },
-      async (request, reply) => {
-        const { telefone, tokenOtp } = request.body
-        const { slug } = request.params
+    },
+    async (request, reply) => {
+      const { telefone, tokenOtp } = request.body
+      const { slug } = request.params
 
-        const usuarioBytelefone = await prisma.usuario.findUnique({
-          where: {
-            numeroCelular: telefone,
-          },
-        })
+      const usuarioBytelefone = await prisma.usuario.findUnique({
+        where: {
+          numeroCelular: telefone,
+        },
+      })
 
-        if (!usuarioBytelefone) {
-          throw new BadRequestError('Usuário não existe com este telefone.')
-        }
+      if (!usuarioBytelefone) {
+        throw new BadRequestError('Usuário não existe com este telefone.')
+      }
 
-        const tokenByUsuarioToken = await prisma.token.findFirst({
-          where: {
-            usuarioId: usuarioBytelefone.id,
-            otpNumber: tokenOtp,
-          },
-        })
+      const tokenByUsuarioToken = await prisma.token.findFirst({
+        where: {
+          usuarioId: usuarioBytelefone.id,
+          otpNumber: tokenOtp,
+        },
+      })
 
-        if (!tokenByUsuarioToken) {
-          throw new UnauthorizedError('Código de acesso inválido.')
-        }
+      if (!tokenByUsuarioToken) {
+        throw new UnauthorizedError('Código de acesso inválido.')
+      }
 
-        const org = await prisma.organizacao.findUnique({
-          where: {
-            slug,
-          },
-        })
+      const org = await prisma.organizacao.findUnique({
+        where: {
+          slug,
+        },
+      })
 
-        const membro = await prisma.membro.findFirst({
-          where: {
+      const membro = await prisma.membro.findFirst({
+        where: {
+          usuarioId: usuarioBytelefone.id,
+          organizacaoId: org!.id,
+        },
+      })
+
+      if (!membro) {
+        await prisma.membro.create({
+          data: {
             usuarioId: usuarioBytelefone.id,
             organizacaoId: org!.id,
+            tipo: 'CLIENTE',
+            role: 'CLIENTE',
           },
         })
+      }
 
-        if (!membro) {
-          await prisma.membro.create({
-            data: {
-              usuarioId: usuarioBytelefone.id,
-              organizacaoId: org!.id,
-              tipo: 'CLIENTE',
-              role: 'CLIENTE',
-            },
-          })
-        }
+      await prisma.token.delete({
+        where: {
+          id: tokenByUsuarioToken.id,
+        },
+      })
 
-        await prisma.token.delete({
-          where: {
-            id: tokenByUsuarioToken.id,
+      const token = await reply.jwtSign(
+        {
+          sub: usuarioBytelefone.id,
+        },
+        {
+          sign: {
+            expiresIn: '7d',
           },
-        })
+        },
+      )
 
-        const token = await reply.jwtSign(
-          {
-            sub: usuarioBytelefone.id,
-          },
-          {
-            sign: {
-              expiresIn: '7d',
-            },
-          },
-        )
-
-        return reply.status(201).send({ token })
-      },
-    )
+      return reply.status(201).send({ token })
+    },
+  )
 }
