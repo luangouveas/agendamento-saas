444bd5f5ca9ade51f1b177f508e114a9907d89fc
Luan Gouveas <lgouveas@rhmed.com.br>
2025-03-02 20:43:34 -0300

    Cria permissoes para concluir e reagendar um agendamento

--- packages/auth/src/subjects/agendamento.ts
+++ packages/auth/src/subjects/agendamento.ts
@@ -8,11 +8,12 @@ export const agendamentoSubject = z.tuple([
     z.literal('get'),
     z.literal('create'),
     z.literal('update'),
+    z.literal('concluir_agendamento'),
     z.literal('confirmar_agendamento'),
     z.literal('cancelar_agendamento'),
     z.literal('reabrir_agendamento'),
     z.literal('transferir_agendamento'),
-    z.literal('concluir_agendamento'),
+    z.literal('reagendar_agendamento'),
   ]),
   z.union([z.literal('Agendamento'), agendamentoSchema]),
 ])
