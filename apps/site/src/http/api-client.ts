bb4f1eeebca6e4219070dc81bcd1e8376d269a05
Luan Gouveas <lgouveas@rhmed.com.br>
2025-03-06 14:35:13 -0300

    Recupera token de autenticação para requisições

--- apps/site/src/http/api-client.ts
+++ apps/site/src/http/api-client.ts
@@ -1,6 +1,28 @@
 import { env } from '@agendamento-saas/env'
+import { getCookie } from 'cookies-next'
+import { CookiesFn } from 'cookies-next/server'
 import ky from 'ky'
 
 export const api = ky.create({
   prefixUrl: env.NEXT_PUBLIC_API_URL,
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        let cookiesStore: CookiesFn | undefined
+
+        if (typeof window === 'undefined') {
+          const { cookies: serverCookies } = await import('next/headers')
+          cookiesStore = serverCookies
+        }
+
+        const token = await getCookie('agendador-token', {
+          cookies: cookiesStore,
+        })
+
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`)
+        }
+      },
+    ],
+  },
 })
