d4a7f5c1957c93856589158a509dafc5a52a557c
Luan Gouveas <lgouveas@rhmed.com.br>
2025-03-07 10:50:17 -0300

    Ajusta nome da rota para escolha de serviços

---
+++ apps/site/src/app/(app)/agendador/[slug]/(pages)/(servicos)/lista-servicos.tsx
@@ -0,0 +1,32 @@
+import { ChevronRight } from 'lucide-react'
+import Link from 'next/link'
+
+import { getSlugOrganizacaoAtual } from '@/app/auth/auth'
+import { buscarServicos } from '@/http/buscar-servicos'
+
+export async function ListaServicos() {
+  const organizacaoAtual = await getSlugOrganizacaoAtual()
+  const { servicos } = await buscarServicos(organizacaoAtual!)
+
+  return (
+    <div className="flex flex-col gap-5">
+      {servicos.map((servico) => (
+        <Link
+          key={servico.id}
+          href={`/agendador/${organizacaoAtual}/profissionais`}
+        >
+          <div className="flex items-center justify-between border-b pb-3">
+            <div className="flex flex-col">
+              <span>{servico.nome}</span>
+              <span className="text-xs">
+                {servico.tempo}min - R$ {servico.valor}
+              </span>
+            </div>
+
+            <ChevronRight size={24} className="text-muted-foreground" />
+          </div>
+        </Link>
+      ))}
+    </div>
+  )
+}
--- apps/site/src/app/(app)/agendador/[slug]/(pages)/(servicos)/page.tsx
+++ apps/site/src/app/(app)/agendador/[slug]/(pages)/(servicos)/page.tsx
@@ -2,11 +2,11 @@ import { ScrollArea } from '@/components/ui/scroll-area'
 
 import { ListaServicos } from './lista-servicos'
 
-export default function HomePage() {
+export default function ServicosPage() {
   return (
-    <div className="space-y-4">
+    <div className="space-y-4 px-4">
       <h2 className="text-center font-semibold">Escolha um serviço</h2>
-      <ScrollArea className="h-[700px] w-full rounded-md p-2">
+      <ScrollArea className="h-[700px] w-full rounded-md">
         <ListaServicos />
       </ScrollArea>
     </div>
