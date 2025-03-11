5d2cc372dc089ae0149144ee12f78891fd1ddef4
Luan Gouveas <lgouveas@rhmed.com.br>
2025-03-10 23:00:04 -0300

    Altera rota de escolher-data para escolher-horario

--- apps/site/src/app/(app)/agendador/[slug]/(pages)/profissionais/lista-profissionais.tsx
+++ apps/site/src/app/(app)/agendador/[slug]/(pages)/profissionais/lista-profissionais.tsx
@@ -25,7 +25,7 @@ export default async function ListaProfissionais(
         profissionais.map((profissional) => (
           <Link
             key={profissional.id}
-            href={`/agendador/${slug}/escolher-data?servicoId=${props.servicoId}&profissionalId=${profissional.membroId}`}
+            href={`/agendador/${slug}/escolher-horario?servicoId=${props.servicoId}&profissionalId=${profissional.membroId}`}
           >
             <div className="flex items-center justify-between border-b pb-3">
               <div className="flex flex-row items-center">
