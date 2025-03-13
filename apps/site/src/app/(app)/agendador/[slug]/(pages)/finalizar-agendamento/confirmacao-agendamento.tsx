35b4fa08d7917647725dbbead42962e28f328a98
Luan Gouveas <lgouveas@rhmed.com.br>
2025-03-13 20:56:57 -0300

    Finaliza layout da tela de confirmação de agendamento cliente

--- apps/site/src/app/(app)/agendador/[slug]/(pages)/finalizar-agendamento/confirmacao-agendamento.tsx
+++ apps/site/src/app/(app)/agendador/[slug]/(pages)/finalizar-agendamento/confirmacao-agendamento.tsx
@@ -25,15 +25,15 @@ export default function ConfirmarAgendamentoClienteForm({
   const [message, setMessage] = useState<string | null>(null)
   const [success, setSuccess] = useState<boolean | null>(null)
 
-  function handleCriarAgendamento(dataForm: FormData) {
+  function handleCriarAgendamento() {
     const [dia, mes, ano] = dadosAgendamento.data.split('/').map(Number)
     const [horas, minutos] = dadosAgendamento.hora.split(':').map(Number)
     const date = new Date(ano, mes - 1, dia, horas - 3, minutos)
     const dataHora = date.toISOString()
 
     finalizarAgendamento({
-      servicoId: dadosAgendamento.servicoId,
-      profissionalId: dadosAgendamento.profissionalId,
+      servicoId: dadosAgendamento.servicoId!,
+      profissionalId: dadosAgendamento.profissionalId!,
       dataHora,
     }).then((result) => {
       if (!result?.success) {
@@ -47,32 +47,43 @@ export default function ConfirmarAgendamentoClienteForm({
   }
 
   return (
-    <div className="space-y-4 px-4">
-      {success && message && (
-        <Alert variant="destructive">
-          <AlertTriangle className="size-4" />
-          <AlertTitle>
-            <p>{message}</p>
-          </AlertTitle>
-        </Alert>
-      )}
-
-      <>
+    <div className="flex h-[580px] flex-col justify-between">
+      <div>
         <h2 className="text-center font-semibold">
           Confirme os dados do agendamento
         </h2>
 
-        <form action={handleCriarAgendamento} className="mt-4">
+        <div className="mt-8">
           <div>{dadosAgendamento.nomeServico}</div>
           <div>{dadosAgendamento.nomeProfissional}</div>
           <div>
             {dadosAgendamento.data} - {dadosAgendamento.hora}
           </div>
-          <div className="mt-4">
-            <Button type="submit">Confirmar agendamento !</Button>
+        </div>
+
+        <div></div>
+
+        {!success && message && (
+          <div className="mt-8 flex w-full items-center justify-center">
+            <Alert variant="destructive" className="max-w-md">
+              <AlertTriangle className="size-4" />
+              <AlertTitle>
+                <p>{message}</p>
+              </AlertTitle>
+            </Alert>
           </div>
-        </form>
-      </>
+        )}
+      </div>
+
+      <div className="bottom-0 mt-4 w-full pb-4">
+        <Button
+          type="submit"
+          onClick={handleCriarAgendamento}
+          className="w-full"
+        >
+          Confirmar agendamento !
+        </Button>
+      </div>
     </div>
   )
 }
