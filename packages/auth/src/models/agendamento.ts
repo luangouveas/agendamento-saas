de07bf64f73007199e0eeee6abed7140be24c390
Luan Gouveas <lgouveas@rhmed.com.br>
2025-03-13 16:29:46 -0300

    Schema do agendamento possui apenas id de serviço e profissional para validação

--- packages/auth/src/models/agendamento.ts
+++ packages/auth/src/models/agendamento.ts
@@ -2,8 +2,6 @@ import { z } from 'zod'
 
 export const agendamentoSchema = z.object({
   __typename: z.literal('Agendamento').default('Agendamento'),
-  id: z.string(),
-  servicoId: z.string(),
   profissionalId: z.string(),
   clienteId: z.string(),
 })
