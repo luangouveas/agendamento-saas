d6df8005b755217036305c6525691e916c57735a
Luan Gouveas <lgouveas@rhmed.com.br>
2025-03-16 15:37:23 -0300

    Inclui comutador de tema na tela de login

--- apps/site/src/app/(app)/agendador/[slug]/sign-in/layout.tsx
+++ apps/site/src/app/(app)/agendador/[slug]/sign-in/layout.tsx
@@ -1,11 +1,18 @@
+import { ComutadorTema } from '@/components/theme/comutador-tema'
+
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center px-4">
-      <div className="w-full max-w-sm">{children}</div>
+    <div>
+      <div className="flex w-full flex-row justify-end border p-2">
+        <ComutadorTema />
+      </div>
+      <div className="flex min-h-screen flex-col items-center justify-center px-4">
+        <div className="w-full max-w-sm">{children}</div>
+      </div>
     </div>
   )
 }
