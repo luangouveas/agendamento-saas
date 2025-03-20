3953b6b2438e8a7185cdcbaa96fd7975400ca0da
Luan Gouveas <lgouveas@rhmed.com.br>
2025-03-20 11:10:25 -0300

    Altera layout do comutador de tema para botão com ação rápida

--- apps/site/src/components/theme/comutador-tema.tsx
+++ apps/site/src/components/theme/comutador-tema.tsx
@@ -4,32 +4,29 @@ import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 
 import { Button } from '../ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '../ui/dropdown-menu'
+
 export function ComutadorTema() {
   const { setTheme } = useTheme()
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
-          <Sun className="size-4 dark:invisible dark:size-0" />
-          <Moon className="invisible size-0 dark:visible dark:size-4" />
-          <span className="sr-only">Mudar tema</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Claro
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Escuro
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div>
+      <Button
+        variant="ghost"
+        size="icon"
+        className="block justify-items-center rounded-xl dark:hidden"
+        onClick={() => setTheme('dark')}
+      >
+        <Sun className="size-5" />
+      </Button>
+
+      <Button
+        variant="ghost"
+        size="icon"
+        className="hidden justify-items-center rounded-xl dark:block"
+        onClick={() => setTheme('light')}
+      >
+        <Moon className="size-5" />
+      </Button>
+    </div>
   )
 }
