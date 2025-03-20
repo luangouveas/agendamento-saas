1e3b49b126b33d437e10a50072714aa8976a09ed
Luan Gouveas <lgouveas@rhmed.com.br>
2025-03-20 11:09:56 -0300

    Altera importações de header e menu

--- apps/site/src/app/(app)/[slug]/(pages)/layout.tsx
+++ apps/site/src/app/(app)/[slug]/(pages)/layout.tsx
@@ -4,30 +4,24 @@ import {
   getSlugOrganizacaoAtual,
   usuarioEstaAutenticado,
 } from '@/app/auth/auth'
-import { AgendadorFooter } from '@/components/agendador-footer'
-import { AgendadorHeader } from '@/components/agendador-header'
-
-type Params = Promise<{ slug: string }>
+import { Header } from '@/components/header'
+import { MenuFlutuante } from '@/components/menu-flutuante'
 
 export default async function AppLayout({
   children,
-  params,
 }: {
   children: React.ReactNode
-  params: Params
 }) {
+  const slug = await getSlugOrganizacaoAtual()
   if (!(await usuarioEstaAutenticado())) {
-    const { slug } = await params
-    return redirect(`/${slug}/sign-in`)
+    return redirect(`/agendador/${slug}/sign-in`)
   }
 
-  const authSlug = await getSlugOrganizacaoAtual()
-
   return (
-    <div>
-      <AgendadorHeader />
-      <main className="w-full pb-20 pt-24">{children}</main>
-      <AgendadorFooter slug={authSlug!} />
+    <div className="space-y-14">
+      <Header />
+      <main className="w-full pb-28 pt-6">{children}</main>
+      <MenuFlutuante slug={slug!} />
     </div>
   )
 }
