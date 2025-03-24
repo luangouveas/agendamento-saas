926860428cb9f2466ed2a1888a3030ed9c006f47
Luan Gouveas <lgouveas@rhmed.com.br>
2025-03-24 16:23:59 -0300

    Verifica se horario é válido antes de criar o agendamento

--- apps/api/src/http/routes/agendamento/criar-agendamento.ts
+++ apps/api/src/http/routes/agendamento/criar-agendamento.ts
@@ -1,4 +1,12 @@
 import { agendamentoSchema } from '@agendamento-saas/auth'
+import {
+  addMinutes,
+  format,
+  isAfter,
+  isBefore,
+  isEqual,
+  parseISO,
+} from 'date-fns'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
@@ -7,6 +15,7 @@ import { auth } from '@/http/middlewares/auth'
 import { prisma } from '@/lib/prisma'
 import { buscarPermissoesUsuario } from '@/utils/buscar-permissoes-usuario'
 
+import { BadRequestError } from '../_errors/bad-request-error'
 import { UnauthorizedError } from '../_errors/unauthorized-error'
 
 export function CriarAgendamento(app: FastifyInstance) {
@@ -22,7 +31,7 @@ export function CriarAgendamento(app: FastifyInstance) {
           security: [{ bearerAuth: [] }],
           body: z.object({
             clienteId: z.string().uuid(),
-            profissionalId: z.string().uuid(),
+            membroId: z.string().uuid(),
             servicoId: z.string().uuid(),
             dataHora: z.string().datetime(),
             valor: z.coerce.number(),
@@ -39,12 +48,37 @@ export function CriarAgendamento(app: FastifyInstance) {
       },
       async (request, reply) => {
         const { slug } = request.params
-        const { clienteId, profissionalId, servicoId, dataHora, valor } =
-          request.body
+        const { clienteId, membroId, servicoId, dataHora, valor } = request.body
 
         const usuarioId = await request.getCurrentUserId()
         const { membership, organizacao } =
           await request.getUserMembership(slug)
+
+        const [servico, membroProfissional] = await Promise.all([
+          prisma.servico.findUnique({ where: { id: servicoId } }),
+          prisma.membro.findUnique({
+            where: { id: membroId },
+            include: {
+              usuario: true,
+              expedientes: {
+                where: { expedientePrincipal: true },
+                include: { diasExpediente: true },
+              },
+            },
+          }),
+        ])
+
+        if (
+          !servico ||
+          !membroProfissional ||
+          !membroProfissional.expedientes
+        ) {
+          throw new BadRequestError(
+            'Desculpe, houve algum problema ao confirmar os dados.',
+          )
+        }
+
+        const profissionalId = membroProfissional.usuarioId
 
         const authAgendamento = agendamentoSchema.parse({
           clienteId,
@@ -60,6 +94,99 @@ export function CriarAgendamento(app: FastifyInstance) {
           throw new UnauthorizedError(
             'Você não possui permissão para criar este agendamento.',
           )
+        }
+
+        const data = addMinutes(new Date(dataHora), -180)
+
+        const expediente = membroProfissional.expedientes[0]
+        const expedienteDia = expediente.diasExpediente.find(
+          (x) => x.diaSemana === data.getDay(),
+        )
+
+        if (!expedienteDia) {
+          throw new BadRequestError('Profissional sem expediente neste dia.')
+        }
+
+        const inicioExpediente = `${format(data, 'yyyy-MM-dd')}T${expedienteDia?.inicio}:00Z`
+        const terminoExpediente = `${format(data, 'yyyy-MM-dd')}T${expedienteDia?.fim}:00Z`
+
+        const inicioDoAgendamentoCandidato = data
+        const terminoDoAgendamentoCandidato = addMinutes(
+          inicioDoAgendamentoCandidato,
+          servico.tempo,
+        )
+
+        // Valida se horario é válido dentro do expediente do profissional
+        if (
+          isBefore(inicioDoAgendamentoCandidato, inicioExpediente) ||
+          isAfter(inicioDoAgendamentoCandidato, terminoExpediente) ||
+          isAfter(terminoDoAgendamentoCandidato, terminoExpediente)
+        ) {
+          throw new BadRequestError(
+            'O horario conflita com o expediente do profissional.',
+          )
+        }
+
+        // Valida se o horario é valido considenrando o intervalo do profissional
+        const hasIntervalo =
+          expedienteDia.inicioIntervalo && expedienteDia.fimIntervalo
+
+        if (hasIntervalo) {
+          const intervaloInicio = parseISO(
+            `${format(data, 'yyyy-MM-dd')}T${expedienteDia.inicioIntervalo}:00Z`,
+          )
+          const intervaloFim = parseISO(
+            `${format(data, 'yyyy-MM-dd')}T${expedienteDia.fimIntervalo}:00Z`,
+          )
+
+          if (
+            (isAfter(inicioDoAgendamentoCandidato, intervaloInicio) ||
+              isEqual(inicioDoAgendamentoCandidato, intervaloInicio)) &&
+            isBefore(inicioDoAgendamentoCandidato, intervaloFim)
+          ) {
+            throw new BadRequestError(
+              'O horario escolhido conflita com o intervalo do profissional',
+            )
+          }
+          if (
+            (isBefore(inicioDoAgendamentoCandidato, intervaloInicio) ||
+              isEqual(inicioDoAgendamentoCandidato, intervaloInicio)) &&
+            isAfter(intervaloFim, intervaloInicio)
+          ) {
+            throw new BadRequestError(
+              'O horario escolhido conflita com o intervalo do profissional',
+            )
+          }
+        }
+
+        const agendamentosDoDia = await prisma.agendamento.findMany({
+          where: {
+            profissionalId,
+            dataHora: {
+              gte: inicioExpediente,
+              lt: terminoExpediente,
+            },
+            status: {
+              notIn: ['CANCELADO'],
+            },
+          },
+          include: { servico: true },
+        })
+
+        // Valida se o horario é valido considerando outros agendamentos para esse profissional
+        if (agendamentosDoDia) {
+          agendamentosDoDia?.forEach((ag) => {
+            const agStart = new Date(ag.dataHora)
+            const agEnd = addMinutes(agStart, ag.servico.tempo)
+            if (
+              inicioDoAgendamentoCandidato < agEnd &&
+              terminoDoAgendamentoCandidato > agStart
+            ) {
+              throw new BadRequestError(
+                'Existe um agendamento causando conflito de horário para este profissional.',
+              )
+            }
+          })
         }
 
         const { id: agendamentoId } = await prisma.agendamento.create({
