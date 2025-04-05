8e7cb3cc4e76f834f8cc4a886277c2586d9be826
Luan Gouveas <lgouveas@rhmed.com.br>
2025-04-05 20:47:27 -0300

    Nomeia rota padrão do agendador para app

--- apps/site/src/app/(app)/[slug]/(pages)/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation'
-
-import { getSlugOrganizacaoAtual } from '@/app/auth/auth'
-
-export default async function HomePage() {
-  const slug = await getSlugOrganizacaoAtual()
-  return redirect(`/${slug}/novo-agendamento`)
-}
