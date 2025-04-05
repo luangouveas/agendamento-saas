eec92bf0396c161efa355252590b7e6198bacf7f
Luan Gouveas <lgouveas@rhmed.com.br>
2025-04-05 20:48:34 -0300

    Centraliza funções de autenticação na raiz do projeto

--- apps/site/src/app/auth/auth.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from 'next/headers'
-
-export async function usuarioEstaAutenticado() {
-  return !!(await cookies()).get('agendador-token')?.value
-}
-
-export async function getSlugOrganizacaoAtual() {
-  return (await cookies()).get('agendador-organizacao')?.value
-}
