ed6c0fbd7d8e8daf0ba4a07a019e2334864ddd66
Luan Gouveas <lgouveas@rhmed.com.br>
2025-04-17 09:12:59 -0300

    Rota de API atualiza perfil sem necessidade de validar org

--- apps/api/src/http/routes/usuario/atualizar-perfil.ts
+++ apps/api/src/http/routes/usuario/atualizar-perfil.ts
@@ -1,34 +1,36 @@
-import { usuarioSchema } from '@agendamento-saas/auth'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
 import { auth } from '@/http/middlewares/auth'
 import { prisma } from '@/lib/prisma'
-import { buscarPermissoesUsuario } from '@/utils/buscar-permissoes-usuario'
 
 import { BadRequestError } from '../_errors/bad-request-error'
-import { UnauthorizedError } from '../_errors/unauthorized-error'
 
 export function AtualizarPerfil(app: FastifyInstance) {
   app
     .withTypeProvider<ZodTypeProvider>()
     .register(auth)
     .put(
-      '/organizacao/:slug/perfil',
+      '/perfil',
       {
         schema: {
           tags: ['Membros'],
           summary: 'Atualiza o perfil do usuário autenticado',
           security: [{ bearerAuth: [] }],
-          params: z.object({
-            slug: z.string(),
-          }),
           body: z.object({
             nome: z.string(),
             email: z.string().email().optional(),
             numeroCelular: z.string(),
-            avatarUrl: z.string().url().optional(),
+            dataNascimento: z
+              .string()
+              .date()
+              .transform((v) => new Date(v)),
+            cep: z.string().optional(),
+            rua: z.string().optional(),
+            bairro: z.string().optional(),
+            cidade: z.string().optional(),
+            estado: z.string().optional(),
           }),
           response: {
             204: z.null(),
@@ -36,27 +38,19 @@ export function AtualizarPerfil(app: FastifyInstance) {
         },
       },
       async (request, reply) => {
-        const { slug } = request.params
-        const { nome, numeroCelular, avatarUrl, email } = request.body
+        const {
+          nome,
+          numeroCelular,
+          email,
+          bairro,
+          cep,
+          cidade,
+          estado,
+          rua,
+          dataNascimento,
+        } = request.body
 
         const usuarioLogadoId = await request.getCurrentUserId()
-        const { membership } = await request.getUserMembership(slug)
-
-        const { cannot } = buscarPermissoesUsuario(
-          usuarioLogadoId,
-          membership.role,
-        )
-
-        const authUsuario = usuarioSchema.parse({
-          id: usuarioLogadoId,
-          role: membership.role,
-        })
-
-        if (cannot('update', authUsuario)) {
-          throw new UnauthorizedError(
-            'Você não possui permissões para atualizar este perfil.',
-          )
-        }
 
         const usuarioComMesmoTelefone = await prisma.usuario.findFirst({
           where: {
@@ -76,7 +70,12 @@ export function AtualizarPerfil(app: FastifyInstance) {
             nome,
             email,
             numeroCelular,
-            avatarUrl,
+            cep,
+            cidade,
+            estado,
+            rua,
+            bairro,
+            dataNascimento,
           },
           where: {
             id: usuarioLogadoId,
