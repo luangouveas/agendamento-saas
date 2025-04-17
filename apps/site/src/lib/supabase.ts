cccbb26ca6925fd25330845a4f31808bf9a2b143
Luan Gouveas <lgouveas@rhmed.com.br>
2025-04-16 21:22:33 -0300

    Ajusta variaveis de ambiente para a lib supabase

--- apps/site/src/lib/supabase.ts
+++ apps/site/src/lib/supabase.ts
@@ -1,7 +1,4 @@
 import { env } from '@agendamento-saas/env'
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  env.NEXT_PUBLIC_SUPABASE_URL,
-  env.NEXT_PUBLIC_SUPABASE_SECRET_KEY,
-)
+export const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_SECRET_KEY)
--- packages/env/index.ts
+++ packages/env/index.ts
@@ -7,12 +7,12 @@ export const env = createEnv({
     JWT_SECRET: z.string(),
     SERVER_PORT: z.coerce.number().default(3031),
     TOKEN_ADMIN: z.string(),
+    SUPABASE_URL: z.string().url(),
+    SUPABASE_SECRET_KEY: z.string(),
   },
   client: {},
   shared: {
     NEXT_PUBLIC_API_URL: z.string().url(),
-    NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
-    NEXT_PUBLIC_SUPABASE_SECRET_KEY: z.string(),
     NEXT_PUBLIC_STRIPE_PUBLISHABE_KEY: z.string(),
     NEXT_PUBLIC_STRIPE_SECRET_KEY: z.string(),
     NEXT_PUBLIC_STRIPE_PLAN_PRICE_FREE_ID: z.string(),
@@ -24,9 +24,8 @@ export const env = createEnv({
     JWT_SECRET: process.env.JWT_SECRET,
     TOKEN_ADMIN: process.env.TOKEN_ADMIN,
     SERVER_PORT: process.env.SERVER_PORT,
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_SECRET_KEY:
-      process.env.NEXT_PUBLIC_SUPABASE_SECRET_KEY,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_SECRET_KEY: process.env.SUPABASE_SECRET_KEY,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_STRIPE_PUBLISHABE_KEY:
       process.env.NEXT_PUBLIC_STRIPE_PUBLISHABE_KEY,
